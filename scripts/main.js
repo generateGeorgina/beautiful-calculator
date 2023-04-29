0a90ef9f63536ffccd858bf2f46bf11cf2db89c0
Georgina Appeagyei <appeagyeigeorgina@gmail.com>
2023-04-29 18:25:33 +0100

    selecting buttons and elements

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,7 @@
-test = document.getElementsByTagName('h1');
-console.log(test);
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
