1c72d6b26e05430bb4a8749a2157e21bdb8cac90
Georgina Appeagyei <appeagyeigeorgina@gmail.com>
2023-05-04 16:55:55 +0100

    Operation buttons and method

--- scripts/main.js
+++ scripts/main.js
@@ -20,13 +20,24 @@ class Calculator {
 
     appendNumber(number) {
         // only one decimal point is allowed
+        // return keyword at the end of if statement's condition is valid syntax (without curly braces)
         if (number === '.' && this.currentOperand.includes('.')) return
         // convert number to string so 1 + 1 is '11' and not 2
         this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
     chooseOperation(operation) {
-
+        // if current operand is empty
+        if (this.currentOperand === '') return
+        // if previous operand has a value, then compute running total
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        // when an operation button is clicked, the operation and number goes to previous operand
+        this.previousOperand = this.currentOperand
+        // then current operand element is cleared
+        this.currentOperand = ''
     }
 
     compute() {
@@ -35,6 +46,7 @@ class Calculator {
 
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand
+        this.previousOperandTextElement.innerText = this.previousOperand
     }
 }
 
@@ -57,6 +69,17 @@ numberButtons.forEach(button => {
         // apeend number method from constructor to equal what ever text is inside the button
         calculator.appendNumber(button.innerText)
         // update display method from constructor to refresh the display with new number
+        calculator.updateDisplay()
+    })
+})
+
+// loop over operation buttons from querySelectorAll
+operationButtons.forEach(button => {
+    // add click event listener for each operation button
+    button.addEventListener('click', () => {
+        // append operation method from constructor to equal what ever text is inside the button
+        calculator.chooseOperation(button.innerText)
+        // update display method from constructor to refresh the display
         calculator.updateDisplay()
     })
 })
